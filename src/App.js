e886e5c39e4025995754dfbc497b632319fe0ba9
Mohammed Ameen <ameerapex496@gmail.com>
2024-09-02 17:26:51 +0200

    added fontawesome to the project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline font-baloo">Hello world!</h1>
+    <>
+      <h1 className="text-3xl font-bold underline font-baloo">Hello world!</h1>
+      <FontAwesomeIcon icon={faCoffee} />
+    </>
   );
 }
