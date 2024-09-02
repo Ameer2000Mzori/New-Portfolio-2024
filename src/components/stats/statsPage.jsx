c7452d9db59dce65ff943225fff14a77f0ee09f2
Mohammed Ameen <ameerapex496@gmail.com>
2024-09-02 20:45:39 +0200

    added stats page

--- src/components/stats/statsPage.jsx
+++ src/components/stats/statsPage.jsx
@@ -1,9 +1,20 @@
 import React from "react";
-
+import { statsData } from "./hooks/statsData";
 export default function StatsPage() {
   return (
-    <div className="h-[40dvh] px-[40px] flex flex-row text-center items-center justify-center gap-[24px]">
-      StatsPage
+    <div className="lg:h-[40dvh] h-auto p-[64px] w-[100dvw] px-[10%] flex flex-col lg:flex-row text-center items-center justify-center gap-[24px]">
+      {statsData.map((el, index) => {
+        return (
+          <div className=" bg-transparent w-[100%] lg:w-[33.33%] border-[#191919]  border-2 p-[24px] rounded-[12px] ">
+            <h1 className=" font-baloo font-bold text-[#B3B3B3] text-[16px]">
+              {el.title}
+            </h1>
+            <h1 className="text-[36px] text-white font-baloo font-bold">
+              {el.describe}
+            </h1>
+          </div>
+        );
+      })}
     </div>
   );
 }
