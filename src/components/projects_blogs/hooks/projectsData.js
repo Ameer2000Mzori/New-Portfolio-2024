902cf16339e4b94e90ec78278eaf1b0c5919ee30
Mohammed Ameen <ameerapex496@gmail.com>
2024-09-03 00:25:30 +0200

    added all data needed and also styling with images

--- src/components/projects_blogs/hooks/projectsData.js
+++ src/components/projects_blogs/hooks/projectsData.js
@@ -1,3 +1,10 @@
+import BankistImg from "../../../assets/images/bankist.png";
+import FrontendcaseImg from "../../../assets/images/frontendcase.png";
+import ShopsPrint from "../../../assets/images/shopsprint.png";
+import TalentPortal from "../../../assets/images/talentportal.png";
+import MonoText from "../../../assets/images/monotext.png";
+import StepsMaster from "../../../assets/images/stepsmaster.png";
+
 export const ProjectTitleData = [
   {
     title: "Crafting Digital Experiences That Matter",
@@ -14,6 +21,7 @@ export const projectsData = [
     description:
       "Bankist-Complete is a sleek website built with HTML, SCSS, and JavaScript. Easily create, log in, delete accounts, transfer money to predefined users, and make money requests. Experience automatic logout for added security when 10 min countdown finishes . Explore the live demo for a hands-on preview.",
     technologies: ["html", "css", "scss", "js"],
+    img: BankistImg,
   },
   {
     title: "Shop Sprint",
@@ -21,6 +29,7 @@ export const projectsData = [
     description:
       "ShopSprint is one of the major websites I've developed using the MERN stack, incorporating the latest packages and technologies. Feel free to explore and download the project to see it in action.",
     technologies: ["react.js", "node.js", "mongodb", "express.js"],
+    img: ShopsPrint,
   },
   {
     title: "frontend case",
@@ -28,6 +37,7 @@ export const projectsData = [
     description:
       "The company 2DIGITS would like a new website where their customers can see their latest blog posts. To this end, Social Brothers created a design to match the client's requirements.",
     technologies: ["next.js", "tailwind.css", "graphql", "prepr"],
+    img: FrontendcaseImg,
   },
   {
     title: "Talent Portal",
@@ -35,6 +45,7 @@ export const projectsData = [
     description:
       "TalentPortal is a website created with Next.js 14, styled with Tailwind CSS, and developed using TypeScript. It utilizes JSON Server for data management. This project serves as a showcase for a job interview.",
     technologies: ["next.js", "Typescript", "tailwind.css"],
+    img: TalentPortal,
   },
   {
     title: "Mono Text",
@@ -48,6 +59,7 @@ export const projectsData = [
       "mongodb.js",
       "express.js",
     ],
+    img: MonoText,
   },
   {
     title: "Steps Master",
@@ -55,5 +67,6 @@ export const projectsData = [
     description:
       "StepsMaster is an application created with ReactNative with help of (Ionic) and nativewind . It will be available soon on Iphone and Samsung",
     technologies: ["react native", "javascript", "ionic", "nativewind"],
+    img: StepsMaster,
   },
 ];
--- src/components/projects_blogs/projects.jsx
+++ src/components/projects_blogs/projects.jsx
@@ -2,12 +2,23 @@ import React from "react";
 import { ProjectTitleData } from "./hooks/projectsData.js";
 export default function ProjectsPage() {
   return (
-    <div className="mt-[32px]">
+    <div className="mt-[32px] flex flex-col text-center items-center justify-center">
       <div className=" mb-[44px]">
-        <h1 className="mb-[16px] text-textColors">
+        <h1 className="mb-[16px] text-upperTitleColor leading-[28px] text-[18px] font-bold font-baloo">
           {ProjectTitleData[0].title}
         </h1>
-        <h1 className="text-textColors">{ProjectTitleData[1].title}</h1>
+        <h1 className="text-textColors leading-[48px] text-[30px] font-bold font-baloo">
+          {ProjectTitleData[1].title}
+        </h1>
+      </div>
+      <div className="w-[80%] flex flex-row flex-wrap text-center items-center justify-center gap-[24px]">
+        <div className="w-[330px] h-[406px] text-textColors flex flex-col text-start items-start justify-start gap-[16px]">
+          <img src={""} alt="" />
+          <h1 className="text-[18px] leading-[28px] font-bold font-baloo text-textColors">
+            aa
+          </h1>
+          <p className=" leading-[24px] text-[16px] font-bold font-baloo">aa</p>
+        </div>
       </div>
     </div>
   );
