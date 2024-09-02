c76841b6f4fefc1b5dc51e27adfaa2e81ec23092
Mohammed Ameen <ameerapex496@gmail.com>
2024-09-02 17:21:41 +0200

    added all custom colors and fonts needed inside tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <h1 className="text-3xl font-bold underline font-baloo">Hello world!</h1>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        baloo: ['"Baloo Thambi 2"', "cursive"],
+      },
+      colors: {
+        cardsBgColors: "#141414",
+        footerBgColor: "#1C1917",
+        darkBgColor: "#242424",
+        upperTitleColor: "#B3B3B3",
+        textColors: "#FFFFFF",
+        buttonsBgColor: "#FF1D48",
+      },
+    },
   },
   plugins: [],
 };
