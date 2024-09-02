79487b593f93e08a6a98556b7f25a895827e9338
Mohammed Ameen <ameerapex496@gmail.com>
2024-09-03 01:02:02 +0200

    added more logic to the project

---
+++ src/components/projects_blogs/components/selectedProject.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function selectedProject() {
+  return (
+    <div className=" fixed top-[50%] left-[50%] w-[80%]">
+      <button>close</button>
+      <img src="" alt="" />
+      <h1></h1>
+      <p></p>
+      <p></p>
+    </div>
+  );
+}
--- src/components/projects_blogs/projects.jsx
+++ src/components/projects_blogs/projects.jsx
@@ -14,19 +14,16 @@ export default function ProjectsPage() {
       <div className="lg:w-[60%] w-[80%] flex flex-row flex-wrap text-center items-center justify-center gap-[24px] ">
         {projectsData.map((el, i) => {
           return (
-            <div
-              key={i}
-              className="w-[330px]  rounded-[4px] bg-cardsBgColors h-auto text-textColors flex flex-col text-start items-start justify-start gap-[16px]"
-            >
+            <div className="w-[330px] hover:drop-shadow-xl cursor-pointer  rounded-[4px] bg-cardsBgColors h-auto pb-[15px] h-min-[550px] text-textColors flex flex-col text-start items-start justify-start gap-[16px]">
               <img
                 src={el.img}
                 alt={`${el.title}`}
                 className="h-[400px] w-full object-cover"
               />
-              <h1 className="text-[18px] pl-[5px] leading-[28px] font-bold font-baloo text-textColors">
+              <h1 className="text-[18px] pl-[15px] leading-[28px] font-bold font-baloo text-textColors">
                 {el.title}
               </h1>
-              <div className="flex flex-row gap-[5px] w-[90%] flex-wrap pl-[5px] mb-[15px]">
+              <div className="flex flex-row gap-[5px] w-[90%] flex-wrap pl-[15px] mb-[15px]">
                 {el.technologies.map((el, i) => {
                   return (
                     <p
@@ -37,6 +34,16 @@ export default function ProjectsPage() {
                     </p>
                   );
                 })}
+              </div>
+              <div className=" gap-[15px] flex flex-row text-center items-center justify-between pr-[15px] pl-[15px] w-[100%] h-[40px]">
+                <a key={i} href={el.url} target="_blank" rel="noreferrer">
+                  <div className=" hover:bg-red-950 bg-buttonsBgColor font-baloo font-bold text-textColors px-[15px] py-[3px] rounded-[5px]">
+                    Project
+                  </div>
+                </a>
+                <div className="  hover:bg-green-900  bg-[#00dc93] font-baloo font-bold  px-[15px] py-[3px] rounded-[5px]">
+                  details
+                </div>
               </div>
             </div>
           );
