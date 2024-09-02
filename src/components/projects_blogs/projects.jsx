c9008b453fb79b7ba1ed0df4df1217e24ee4e604
Mohammed Ameen <ameerapex496@gmail.com>
2024-09-03 01:41:55 +0200

    added logic now the user can select each project he wants to

--- src/components/projects_blogs/projects.jsx
+++ src/components/projects_blogs/projects.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { ProjectTitleData, projectsData } from "./hooks/projectsData.js";
+import SelectedProject from "./components/SelectedProject.jsx";
 export default function ProjectsPage() {
+  const [selectedPorjectData, setSelectedProjectData] = React.useState({});
+  const [isSelected, setIsSelected] = React.useState(false);
+
   return (
     <div className="mt-[32px]  mb-[64px] flex flex-col text-center items-center justify-center">
       <div className=" mb-[44px]">
@@ -12,19 +16,19 @@ export default function ProjectsPage() {
         </h1>
       </div>
       <div className="lg:w-[60%] w-[80%] flex flex-row flex-wrap text-center items-center justify-center gap-[24px] ">
-        {projectsData.map((el, i) => {
+        {projectsData.map((data, i) => {
           return (
             <div className="w-[330px] hover:drop-shadow-xl cursor-pointer  rounded-[4px] bg-cardsBgColors h-auto pb-[15px] h-min-[550px] text-textColors flex flex-col text-start items-start justify-start gap-[16px]">
               <img
-                src={el.img}
-                alt={`${el.title}`}
+                src={data.img}
+                alt={`${data.title}`}
                 className="h-[400px] w-full object-cover"
               />
               <h1 className="text-[18px] pl-[15px] leading-[28px] font-bold font-baloo text-textColors">
-                {el.title}
+                {data.title}
               </h1>
               <div className="flex flex-row gap-[5px] w-[90%] flex-wrap pl-[15px] mb-[15px]">
-                {el.technologies.map((el, i) => {
+                {data.technologies.map((el, i) => {
                   return (
                     <p
                       key={i}
@@ -36,12 +40,26 @@ export default function ProjectsPage() {
                 })}
               </div>
               <div className=" gap-[15px] flex flex-row text-center items-center justify-between pr-[15px] pl-[15px] w-[100%] h-[40px]">
-                <a key={i} href={el.url} target="_blank" rel="noreferrer">
+                <a key={i} href={data.url} target="_blank" rel="noreferrer">
                   <div className=" hover:bg-red-950 bg-buttonsBgColor font-baloo font-bold text-textColors px-[15px] py-[3px] rounded-[5px]">
                     Project
                   </div>
                 </a>
-                <div className="  hover:bg-green-900  bg-[#00dc93] font-baloo font-bold  px-[15px] py-[3px] rounded-[5px]">
+                <div
+                  onClick={() => {
+                    const { title, description, img, url, technologies } = data;
+                    console.log("data", data);
+                    setSelectedProjectData({
+                      title,
+                      description,
+                      img,
+                      url,
+                      technologies,
+                    });
+                    setIsSelected(true);
+                  }}
+                  className=" hover:bg-green-900  bg-[#00dc93] font-baloo font-bold  px-[15px] py-[3px] rounded-[5px]"
+                >
                   details
                 </div>
               </div>
@@ -49,6 +67,12 @@ export default function ProjectsPage() {
           );
         })}
       </div>
+      {isSelected && (
+        <SelectedProject
+          setIsSelectedProject={setIsSelected}
+          projectData={selectedPorjectData}
+        />
+      )}
     </div>
   );
 }
